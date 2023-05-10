9c4d081b147cf21f3f0c2f529cee962bc51ccd6c
bhardwajrizul <67204311+bhardwajrizul@users.noreply.github.com>
2023-05-10 19:27:37 +0000

    Responsive NotFound Page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './NotFound.css';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const NotFound = () => {
   return (
     <>
       <Navbar />
-      <div className='container' href="" target="_blank">
+      <div className='container-not-found' href="" target="_blank">
         <header className="top-header"></header>
 
         {/* Dust particel */}
@@ -20,31 +22,39 @@ const NotFound = () => {
         {/* Dust particle end */}
 
         <div className="lamp__wrap">
-          <div className="lamp">
-            <div className="cable"></div>
-            <div className="cover"></div>
-            <div className="in-cover">
-              <div className="bulb"></div>
+          <div className="light-container">
+            <div className="lamp">
+              <div className="cable"></div>
+              <div className="cover"></div>
+              <div className="in-cover">
+                <div className="bulb"></div>
+              </div>
+              <div className="light"></div>
             </div>
-            <div className="light"></div>
           </div>
         </div>
         {/* END Lamp */}
-        
+
         <section className="error">
           {/* Content */}
           <div className="error__content">
             <div className="error__message message text">
               <h1 className="message__title">Page Not Found</h1>
-              <p className="message__text">We're sorry, the page you were looking for isn't found here. The link you followed may either be broken or no longer exists. Please try again, or take a look at our.</p>
+              <p className="message__text">We're sorry, the page you were looking for doesn't exist.</p>
             </div>
             <div className="error__nav e-nav">
-              <a href="" target="_blanck" className="e-nav__link"></a>
+              <Link
+                to="/"
+                className="btn btn-outline mb-[12rem]"
+              >
+                Homepage
+              </Link>
             </div>
           </div>
           {/* END Content */}
         </section>
       </div>
+      <Footer />
     </>
   );
 };
